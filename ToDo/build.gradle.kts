8a814911a7af6bd7289734b54bfb4db3bb1fc4c7
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-06-14 18:15:39 +0300

    8.2.0-alpha08 work Kotlin 1.9.0-Beta Plugins hilt 2.46.1 _14.06.2023  gradle 8.2-rc-2 Android 34

--- ToDo/build.gradle.kts
+++ ToDo/build.gradle.kts
@@ -4,8 +4,8 @@
 // _14.03.2023 canary 8 + gradle 8.0.2
 plugins {
     // Объявляются классы Gradle, которые нужны для сборки зависимостей
-    id ("com.android.application") version "8.2.0-alpha07" apply false
-    id ("com.android.library") version "8.2.0-alpha07" apply false
+    id ("com.android.application") version "8.2.0-alpha08" apply false
+    id ("com.android.library") version "8.2.0-alpha08" apply false
     id ("org.jetbrains.kotlin.android") version "1.9.0-Beta" apply false  // 1.9.0-Beta 1.8.21
     // Нужно для передачи параметров в навигации проекта
     id ("androidx.navigation.safeargs.kotlin")  version "2.7.0-beta01" apply false   //  2.7.0-alpha01 2.6.0-rc02

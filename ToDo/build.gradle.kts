e618a30ce12f2b5ccabf5ef6dd4d1a85c8d5c322
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-12-24 15:09:26 +0300

    8.3.0-alpha18 work Kotlin2.0.0-Beta2-1.0.16 hilt 2.50 ksp _21.12.2023  gradlegradle-8.6-milestone-1 Android 34 androidx.navigation 2.7.6 little upgrade

--- ToDo/build.gradle.kts
+++ ToDo/build.gradle.kts
@@ -4,13 +4,13 @@
 // _14.03.2023 canary 8 + gradle 8.0.2
 plugins {
     // Объявляются классы Gradle, которые нужны для сборки зависимостей
-    id ("com.android.application") version "8.3.0-alpha16" apply false
-    id ("com.android.library") version "8.3.0-alpha16" apply false
+    id ("com.android.application") version "8.3.0-alpha18" apply false
+    id ("com.android.library") version "8.3.0-alpha18" apply false
 //    id ("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id ("org.jetbrains.kotlin.android.extensions") version "2.0.0-Beta1" apply false
+    id ("org.jetbrains.kotlin.android.extensions") version "2.0.0-Beta2" apply false
     // Нужно для передачи параметров в навигации проекта
-    id ("androidx.navigation.safeargs.kotlin")  version "2.7.5" apply false
+    id ("androidx.navigation.safeargs.kotlin")  version "2.7.6" apply false
     // Нужно для внедрения зависимостей hilt
-    id ("com.google.dagger.hilt.android")  version "2.48.1" apply false
-    id ("com.google.devtools.ksp") version "2.0.0-Beta1-1.0.14" apply false
+    id ("com.google.dagger.hilt.android")  version "2.50" apply false
+    id ("com.google.devtools.ksp") version "2.0.0-Beta2-1.0.16" apply false
 }
